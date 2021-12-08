818521e93313a418a3f853e308168b8f1b7bb4a8
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-12-07 16:10:01 -0800

    added filter by name

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import { getCountries } from './services/countries';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getCountries();
+      setCountries(data);
+    };
+    fetchData();
+  }, []);
+
+  function filterCountries() {
+    return countries.filter((country) => {
+      return country.name.includes(query);
+    });
+  }
+
   return (
     <div className="App">
       <h1>Flags of the World</h1>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+      />
+      {filterCountries().map((c) => {
+        return (
+          <>
+            <div key="c.id" className="name">
+              {c.name};
+            </div>
+            <img src={`https://flagcdn.com/80x60/${c.iso2.toLowerCase()}.png`}></img>
+          </>
+        );
+      })}
     </div>
   );
 }
